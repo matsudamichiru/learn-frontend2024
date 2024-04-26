b6f805c95ae0c497c48c48294de9440f09b3896a
vogaro-matsuda <124142889+matsudamichiru@users.noreply.github.com>
2024-04-26 15:17:32 +0900

    viteの導入

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
+  base: '/learn-frontend2024/',
   root,
   build: {
     outDir,
