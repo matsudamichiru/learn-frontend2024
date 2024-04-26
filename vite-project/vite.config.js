9e5d3e32bd3ef845da880d2ffd06264946e30ce0
vogaro-matsuda <124142889+matsudamichiru@users.noreply.github.com>
2024-04-26 14:37:03 +0900

    viteの導入

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,8 +1,17 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-const root = resolve(__dirname, 'src')
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
-	root,
-})
+  root,
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        sample1: resolve(root, 'sample1', 'index.html')
+      },
+    },
+  },
+});
