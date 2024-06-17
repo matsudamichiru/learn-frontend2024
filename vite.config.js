6bbd8c525391fd194660bbe3615f3d1915f8c592
vogaro-matsuda <124142889+matsudamichiru@users.noreply.github.com>
2024-06-17 17:39:50 +0900

    cssブレンドモードテスト

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,9 @@ export default defineConfig({
       input: {
         sample1: resolve(root, 'sample1', 'index.html'),
         sample2: resolve(root, 'sample2', 'index.html'),
-        sample3: resolve(root, 'sample3', 'index.html')
+        sample3: resolve(root, 'sample3', 'index.html'),
+        sample4: resolve(root, 'sample4', 'index.html'),
+        sample5: resolve(root, 'sample5', 'index.html')
       },
     },
   },
